b1cb8869fa6c2bf3f885a2b41c5979b1c5149b09
EShip <shipelin.e@yandex.ru>
2023-04-04 23:02:14 +0300

    dotenv

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cors = require('cors');
-//  require('dotenv').config();
+require('dotenv').config();
 
 const { PORT = 3001 } = process.env;
 const app = express();
